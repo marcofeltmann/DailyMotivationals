22ffa99e362f938f05f99494a6ab7f779feed45a
Marco Feltmann <coding@marcofeltmann.de>
2020-02-12 16:59:27 +0100

    Initially set up project.

---
+++ Daily Motivational/MotivationalCore/MotivationalCore.h
@@ -0,0 +1,19 @@
+//
+//  MotivationalCore.h
+//  MotivationalCore
+//
+//  Created by Marco Feltmann on 12.02.20.
+//  Copyright © 2020 mfs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MotivationalCore.
+FOUNDATION_EXPORT double MotivationalCoreVersionNumber;
+
+//! Project version string for MotivationalCore.
+FOUNDATION_EXPORT const unsigned char MotivationalCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MotivationalCore/PublicHeader.h>
+
+
